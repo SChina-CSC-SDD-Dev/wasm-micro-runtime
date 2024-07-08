c28fc02429c7f996e39fece937cdcc45d30f134c
Zhang Yang <SChina-CSC-SDD-Dev@ap.sony.com>
2024-07-08 10:59:15 +0800

    Revert "aot: Make precheck functions use short-call for xtensa (#3418)"
    This reverts commit 8f098a5905d4b9a741933ffdb8a75f493e4e81d8.

--- build-scripts/build_llvm.py
+++ build-scripts/build_llvm.py
@@ -265,7 +265,7 @@ def main():
         "xtensa": {
             "repo": "https://github.com/espressif/llvm-project.git",
             "repo_ssh": "git@github.com:espressif/llvm-project.git",
-            "branch": "xtensa_release_17.0.1",
+            "branch": "xtensa_release_15.x",
         },
         "default": {
             "repo": "https://github.com/llvm/llvm-project.git",
